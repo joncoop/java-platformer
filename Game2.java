db61f38b1bb2b7cdcdb0a9e3d4617c9400dd229f
joncoop <joncoop1972@gmail.com>
2016-02-17 14:46:33 -0500

    Made a window.

---
+++ Game2.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Application here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Game2 extends JPanel {
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setColor(Color.RED);	
+		g2d.fillRect(50, 50, 30, 30);
+
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("My Game");
+		frame.add(new Game2());
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
