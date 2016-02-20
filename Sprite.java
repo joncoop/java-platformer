a88eed7555d97ef8e45b79784f6714c4d6953c64
joncoop <joncoop1972@gmail.com>
2016-02-20 15:24:56 -0500

    Completely reworked Sprite class. Scrapped Rectangle. Using built-in.

--- Sprite.java
+++ Sprite.java
@@ -6,21 +6,57 @@
  * @version (version number or date here)
  */
 
+
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public abstract class Sprite
 {
+    private BufferedImage img;
     private Rectangle rect;
     
-    public Sprite(Rectangle rect)
+    public Sprite(int x, int y, BufferedImage img)
     {
-        this.rect = rect;
+        int w = img.getWidth();
+        int h = img.getHeight();
+        
+        this.img = img;
+        this.rect = new Rectangle(x, y, w, h);
+    }
+    
+    public void move(int dx, int dy)
+    {
+        rect.translate(dx, dy);
+    }
+    
+    public void moveTo(int x, int y)
+    {
+        rect.setLocation(x, y);
+    }
+    
+    public boolean collidesWith(Sprite other)
+    {
+        return this.rect.intersects(other.rect);
+    }
+    
+    public ArrayList<Sprite> getCollisionList(ArrayList<Sprite> spriteList) {
+       ArrayList<Sprite> collidedSprites = new ArrayList<Sprite>();
+       
+       for (Sprite s : spriteList)
+           if (this.collidesWith(s))
+               collidedSprites.add(s);
+        
+       return collidedSprites;
+    }
+    
+    public void paint(Graphics g)
+    {
+        g.drawImage(img, (int)rect.getX(), (int)rect.getY(), (int)rect.getWidth(), (int)rect.getHeight(), null);   
     }
     
     public abstract void update();
-    
-    public abstract void paint(Graphics g);
-    
 
 }
